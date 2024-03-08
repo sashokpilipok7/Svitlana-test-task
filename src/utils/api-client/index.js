319c9fffd1584786f5337c166717e9523be4f371
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2024-03-08 22:50:17 +0200

    feat: api utils

---
+++ src/utils/api-client/index.js
@@ -0,0 +1,50 @@
+export const RESPONSE_STATUSES = {
+  UNAUTORIZED: 401,
+};
+
+export class ApiClient {
+  constructor() {
+    this.baseUrl = process.env.API_URL;
+  }
+
+  _request({ method, url, data, config = {} }) {
+    return new Promise((resolve, reject) => {
+      let payload = {
+        method,
+        headers: {
+          ...config.headers,
+        },
+        ...config,
+      };
+
+      fetch(this.baseUrl + url, payload)
+        .then((response) => {
+          return Promise.all([
+            response.json(),
+            response.status === RESPONSE_STATUSES.UNAUTORIZED,
+          ]);
+        })
+        .then((values) => {
+          const [json, isUnAutorized] = values;
+          if (!isUnAutorized) {
+            resolve(json);
+          }
+        })
+        .catch((err) => {
+          toast.error(err);
+          reject(err);
+        });
+    });
+  }
+
+  get(url, params, config) {
+    return this._request({
+      method: "GET",
+      url,
+      data: params,
+      config,
+    });
+  }
+}
+
+export default new ApiClient();
