12c7fbed1bce012e0647f6dfe9e0abaadd1912f5
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2024-03-08 22:38:43 +0200

    feat: init app

---
+++ src/containers/App/index.js
@@ -0,0 +1,16 @@
+import "./styles.scss";
+
+function App() {
+  return (
+    <>
+      <main className="app">
+        <div className="bg-gray-200 p-4 rounded-lg shadow-md">
+          <p className="text-blue-500 font-bold">Hello, Tailwind CSS!</p>
+        </div>
+        <button className="text-red-500 bg-cyan-600 p-6">HaAA</button>
+      </main>
+    </>
+  );
+}
+
+export default App;
